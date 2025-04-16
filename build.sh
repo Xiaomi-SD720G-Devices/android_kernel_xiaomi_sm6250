ad7f2badf46ff7b0866d0197f9b4b5c2f5640959
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-04-16 13:47:24 +0000

    Update build scripts
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- build.sh
+++ build.sh
@@ -1,284 +1,191 @@
 #!/usr/bin/env bash
 
- #
- # Script For Building Android Kernel
- #
+#
+# Script For Building Android Kernel
+#
 
 ##----------------------------------------------------------##
 # Specify Kernel Directory
 KERNEL_DIR="$(pwd)"
 
-##----------------------------------------------------------##
 # Device Name and Model
 MODEL=Xiaomi
 DEVICE=Miatoll
 
-# Kernel Version Code
-VERSION=
-
 # Kernel Defconfig
 DEFCONFIG=cust_defconfig
 
-# Select LTO variant ( Full LTO by default )
-DISABLE_LTO=0
-THIN_LTO=0
-
 # Files
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
-DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
-DTB=$(pwd)/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb
+IMAGE=${KERNEL_DIR}/out/arch/arm64/boot/Image.gz
+DTBO=${KERNEL_DIR}/out/arch/arm64/boot/dtbo.img
+DTB=${KERNEL_DIR}/out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb
 
 # Verbose Build
 VERBOSE=0
 
 # Kernel Version
 KERVER=$(make kernelversion)
-
 COMMIT_HEAD=$(git log --oneline -1)
 
 # Date and Time
 DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
 TANGGAL=$(date +"%F%S")
 
-# Specify Final Zip Name
-ZIPNAME=Forza
+# Final Zip Name
+ZIPNAME=RedCherry
 FINAL_ZIP=${ZIPNAME}-kernel-v${KERVER}-${DEVICE}-${TANGGAL}.zip
 
-##----------------------------------------------------------##
-# Specify compiler.
-
-COMPILER=azure
-
-##----------------------------------------------------------##
-# Specify Linker
+# Compiler and Linker
+COMPILER=aosp
 LINKER=ld.lld
 
-##----------------------------------------------------------##
+# Log File
+LOG_FILE="log.txt"
 
-##----------------------------------------------------------##
+##------------------------------------------------------##
+# Truncate log file at the start
+: > "$LOG_FILE"
+
+# Prevent Terminal Auto-Close on Errors
+trap 'echo "Script finished. Press any key to exit..."; read -n 1' EXIT
+
+##------------------------------------------------------##
+# Clean Existing Files and Kernel Build Environment
+function clean_previous_files() {
+    echo "Cleaning previous kernel files and build environment..." | tee -a "$LOG_FILE"
+
+    # Remove old zip files
+    rm -f AnyKernel3/RedCherry-kernel*.zip
+
+    # Remove previous kernel build outputs in AnyKernel3
+    rm -f AnyKernel3/Image.gz AnyKernel3/dtbo.img
+    rm -rf AnyKernel3/dtb/*
+
+    # Perform a clean build: make clean and make mrproper
+    make clean && make mrproper | tee -a "$LOG_FILE"
+}
+
+##------------------------------------------------------##
 # Clone ToolChain
 function cloneTC() {
+    echo "Cloning toolchain for compiler: $COMPILER" | tee -a "$LOG_FILE"
 
-	if [ $COMPILER = "atomx" ];
-	then
-	git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
+    if [ ! -d "${KERNEL_DIR}/clang" ]; then
+        mkdir clang && cd clang || exit 1
+        echo "Downloading AOSP clang..."
+        wget "https://github.com/userariii/AOSP-clang/releases/download/clang-r530567/clang-r530567.tar.gz"
+        tar -xvf clang*
+        cd .. || exit 1
+    else
+        echo "Directory 'clang' already exists. Skipping download." | tee -a "$LOG_FILE"
+    fi
 
-        elif [ $COMPILER = "neutron" ];
-        then
-        git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git clang
-        PATH="${KERNEL_DIR}/clang/bin:$PATH"
+    if [ ! -d "${KERNEL_DIR}/gcc" ]; then
+        git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
+    fi
 
-	elif [ $COMPILER = "azure" ];
-	then
-	git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
+    if [ ! -d "${KERNEL_DIR}/gcc32" ]; then
+        git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git --depth=1 gcc32
+    fi
 
-	elif [ $COMPILER = "proton" ];
-	then
-	git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-	PATH="${KERNEL_DIR}/clang/bin:$PATH"
+    PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:$PATH"
 
-	elif [ $COMPILER = "eva" ];
-	then
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git -b gcc-new gcc64
-	git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git -b gcc-new gcc32
-	PATH=$KERNEL_DIR/gcc64/bin/:$KERNEL_DIR/gcc32/bin/:/usr/bin:$PATH
+    if [ ! -d AnyKernel3 ]; then
+        git clone --depth=1 https://github.com/userariii/AnyKernel3.git -b master
+    else
+        echo "Directory 'AnyKernel3' already exists. Skipping download." | tee -a "$LOG_FILE"
+    fi
+}
 
-	elif [ $COMPILER = "aosp" ];
-	then
-        mkdir aosp-clang
-        cd aosp-clang || exit
-	wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r450784b.tar.gz
-        tar -xf clang*
-        cd .. || exit
-	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
-	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
-	PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-	fi
-        # Clone AnyKernel
-        git clone --depth=1  https://github.com/userariii/AnyKernel3.git -b Forza
+##------------------------------------------------------##
+# Determine the number of threads for compilation
+function determine_threads() {
+    echo "Determining number of threads..." | tee -a "$LOG_FILE"
+    if [ "$(cat /sys/devices/system/cpu/smt/active)" = "1" ]; then
+        THREADS=$(expr $(nproc --all) \* 2)
+    else
+        THREADS=$(nproc --all)
+    fi
+    echo "Number of threads: $THREADS" | tee -a "$LOG_FILE"
+}
 
-	}
-	
 ##------------------------------------------------------##
 # Export Variables
 function exports() {
-	
-        # Export KBUILD_COMPILER_STRING
-        if [ -d ${KERNEL_DIR}/clang ];
-           then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-        elif [ -d ${KERNEL_DIR}/gcc64 ];
-           then
-               export KBUILD_COMPILER_STRING=$("$KERNEL_DIR/gcc64"/bin/aarch64-elf-gcc --version | head -n 1)
-        elif [ -d ${KERNEL_DIR}/aosp-clang ];
-            then
-               export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/aosp-clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-        fi
-        
-        # Export ARCH and SUBARCH
-        export ARCH=arm64
-        export SUBARCH=arm64
-        
-        # Export Local Version
-        export LOCALVERSION="-${VERSION}"
-        
-        # KBUILD HOST and USER
-        export KBUILD_BUILD_HOST=Linux
-        export KBUILD_BUILD_USER="CRUECY"
-        
-        # CI
-        if [ "$CI" ]
-           then
-               
-           if [ "$CIRCLECI" ]
-              then
-                  export KBUILD_BUILD_VERSION=${CIRCLE_BUILD_NUM}
-                  export CI_BRANCH=${CIRCLE_BRANCH}
-           elif [ "$DRONE" ]
-	      then
-		  export KBUILD_BUILD_VERSION=${DRONE_BUILD_NUMBER}
-		  export CI_BRANCH=${DRONE_BRANCH}
-           fi
-		   
-        fi
-	export PROCS=$(nproc --all)
-	export DISTRO=$(source /etc/os-release && echo "${NAME}")
-	}
-        
-##----------------------------------------------------------------##
-# Telegram Bot Integration
+    if [ -d "${KERNEL_DIR}/clang" ]; then
+        export KBUILD_COMPILER_STRING=$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1)
+    fi
 
-function post_msg() {
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-	-d chat_id="$chat_id" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-	}
+    export ARCH=arm64
+    export SUBARCH=arm64
+    export LOCALVERSION="-${VERSION}"
+    export KBUILD_BUILD_HOST=Linux
+    export KBUILD_BUILD_USER="CRUECY"
+    export DISTRO=$(source /etc/os-release && echo "${NAME}")
+}
 
-function push() {
-	curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
-	-F chat_id="$chat_id" \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2"
-	}
-##----------------------------------------------------------------##
-# Export Configs
-function configs() {
-    if [ -d ${KERNEL_DIR}/clang ] || [ -d ${KERNEL_DIR}/aosp-clang  ]; then
-       if [ $DISABLE_LTO = "1" ]; then
-          sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/' arch/arm64/configs/cust_defconfig
-          sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/' arch/arm64/configs/cust_defconfig
-          sed -i 's/# CONFIG_LTO_NONE is not set/CONFIG_LTO_NONE=y/' arch/arm64/configs/cust_defconfig
-       elif [ $THIN_LTO = "1" ]; then
-          sed -i 's/# CONFIG_THINLTO is not set/CONFIG_THINLTO=y/' arch/arm64/configs/cust_defconfig
-       fi
-    elif [ -d ${KERNEL_DIR}/gcc64 ]; then
-       sed -i 's/CONFIG_LLVM_POLLY=y/# CONFIG_LLVM_POLLY is not set/' arch/arm64/configs/cust_defconfig
-       sed -i 's/# CONFIG_GCC_GRAPHITE is not set/CONFIG_GCC_GRAPHITE=y/' arch/arm64/configs/cust_defconfig
-       if ! [ $DISABLE_LTO = "1" ]; then
-          sed -i 's/# CONFIG_LTO_GCC is not set/CONFIG_LTO_GCC=y/' arch/arm64/configs/cust_defconfig
-       fi
+##------------------------------------------------------##
+# Compile the Kernel
+function compile() {
+    START=$(date +"%s")
+    echo "Starting compilation..." | tee -a "$LOG_FILE"
+
+    make O=out $DEFCONFIG | tee -a "$LOG_FILE"
+
+    make -kj$THREADS O=out ARCH=arm64 CC=clang \
+        CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- HOSTCC=clang HOSTCXX=clang++ \
+        HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" LD=$LINKER LLVM=1 LLVM_IAS=1 AR=llvm-ar \
+        NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip READELF=llvm-readelf OBJSIZE=llvm-size \
+        V=$VERBOSE 2>&1 | tee -a "$LOG_FILE"
+
+    END=$(date +"%s")
+    DIFF=$((END - START))
+    echo "Compilation took $DIFF seconds." | tee -a "$LOG_FILE"
+}
+
+##------------------------------------------------------##
+# Zip the Kernel with Exception Handling
+function zipping() {
+    echo "Starting zipping process..." | tee -a "$LOG_FILE"
+
+    # Ensure the dtb directory exists
+    mkdir -p AnyKernel3/dtb
+
+    # Remove any existing zip file with the same name to avoid duplicates
+    if [ -f "AnyKernel3/$FINAL_ZIP" ]; then
+        echo "Removing existing zip: $FINAL_ZIP" | tee -a "$LOG_FILE"
+        rm "AnyKernel3/$FINAL_ZIP"
+    fi
+
+    if [[ -f $IMAGE && -f $DTBO && -f $DTB ]]; then
+        cp "$IMAGE" AnyKernel3/
+        cp "$DTBO" AnyKernel3/
+        cp "$DTB" AnyKernel3/dtb/
+        cd AnyKernel3 || exit 1
+
+        # Create the zip only once
+        zip -r9 "$FINAL_ZIP" * | tee -a "../$LOG_FILE"
+        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
+        echo "MD5: $MD5CHECK" | tee -a "../$LOG_FILE"
+        cd ..
+    else
+        echo "Error: One or more required files are missing!" | tee -a "$LOG_FILE"
+        [[ ! -f $IMAGE ]] && echo "Missing: $IMAGE" | tee -a "$LOG_FILE"
+        [[ ! -f $DTBO ]] && echo "Missing: $DTBO" | tee -a "$LOG_FILE"
+        [[ ! -f $DTB ]] && echo "Missing: $DTB" | tee -a "$LOG_FILE"
+        exit 1
     fi
 }
-##----------------------------------------------------------##
-# Compilation
-function compile() {
-START=$(date +"%s")
-	# Push Notification
-	post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
-	
-	# Compile
-	make O=out ${DEFCONFIG}
-	if [ -d ${KERNEL_DIR}/clang ];
-	   then
-	       make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CC=clang \
-	       HOSTCC=clang \
-	       HOSTCXX=clang++ \
-	       CROSS_COMPILE=aarch64-linux-gnu- \
-	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-	       LD=${LINKER} \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       READELF=llvm-readelf \
-	       OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-	elif [ -d ${KERNEL_DIR}/gcc64 ];
-	   then
-	       make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CROSS_COMPILE_ARM32=arm-eabi- \
-	       CROSS_COMPILE=aarch64-elf- \
-	       LD=aarch64-elf-${LINKER} \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
-	       OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-        elif [ -d ${KERNEL_DIR}/aosp-clang ];
-           then
-               make -kj$(nproc --all) O=out \
-	       ARCH=arm64 \
-	       CC=clang \
-               HOSTCC=clang \
-	       HOSTCXX=clang++ \
-	       CLANG_TRIPLE=aarch64-linux-gnu- \
-	       CROSS_COMPILE=aarch64-linux-android- \
-	       CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-	       LD=${LINKER} \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-               STRIP=llvm-strip \
-	       READELF=llvm-readelf \
-	       OBJSIZE=llvm-size \
-	       V=$VERBOSE 2>&1 | tee error.log
-	fi
-	
-	# Verify Files
-	if ! [ -a "$IMAGE" ];
-	   then
-	       push "error.log" "Build Throws Errors"
-	       exit 1
-	   else
-	       post_msg " Kernel Compilation Finished. Started Zipping "
-	fi
-	}
 
-##----------------------------------------------------------------##
-function zipping() {
-	# Copy Files To AnyKernel3 Zip
-	cp $IMAGE AnyKernel3
-	cp $DTBO AnyKernel3
-        cp $DTB AnyKernel3/dtb
-	
-	# Zipping and Push Kernel
-	cd AnyKernel3 || exit 1
-        zip -r9 ${FINAL_ZIP} *
-        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-        push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-        cd ..
-        }
-    
-##----------------------------------------------------------##
+##------------------------------------------------------##
+# Execute Functions
+clean_previous_files   # Clean up before building
+cloneTC                # Clone necessary toolchains
+determine_threads      # Determine the number of threads
+exports                # Export necessary variables
+compile                # Compile the kernel
+zipping                # Zip the compiled files
 
-cloneTC
-exports
-configs
-compile
-END=$(date +"%s")
-DIFF=$(($END - $START))
-zipping
-
-##----------------*****-----------------------------##
+# Keep the terminal open after completion
+echo "Script completed successfully. Press any key to exit..." | tee -a "$LOG_FILE"
+read -n 1
